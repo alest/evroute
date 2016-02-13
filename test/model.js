a9cbc0d290847bb94f7c1219ceaa47d229c5524d
Francis Giraldeau <francis.giraldeau@gmail.com>
2016-02-12 19:09:54 -0500

    Stuff
    Signed-off-by: Francis Giraldeau <francis.giraldeau@gmail.com>

--- test/model.js
+++ test/model.js
@@ -22,6 +22,33 @@ var chargerCoordinates = function(entry) {
     return [entry.LatLng.Lat, entry.LatLng.Lng];
 };
 
+var filterFastChargers = function(chargers) {
+    return chargers.filter(function (elem) {
+        return elem.Level === 3;
+    });
+};
+
+var findChargerByUid = function(chargers, uid) {
+    for (var i = 0; i < chargers.length; i++) {
+        var ids = chargers[i].Ids;
+        for (var j = 0; j < ids.length; j++) {
+            if (ids[j] === uid)
+                return chargers[i];
+        }
+        return undefined;
+    }
+    // Cannot call find on the charger array, mystery
+    /*
+    return chargers.find(function(elem) {
+        for (var i = 0; i < elem.Ids.length; i++) {
+            if (elem.Ids[i] === uid)
+                return true;
+        }
+        return false;
+    });
+    */
+};
+
 describe("basic", function() {
     it("simple graph", function() {
         var cy = cytoscape();
@@ -82,4 +109,39 @@ describe("basic", function() {
             }
         });
     });
+
+    it("charger graph", function(done) {
+        this.timeout(10000);
+        loadChargers(dataPath, function(chargers) {
+            var quickChargers = filterFastChargers(chargers);
+            console.log(quickChargers.length);
+
+            var counter = 0;
+            var cy = cytoscape();
+            for (var i = 0; i < quickChargers.length; i++) {
+                cy.add({ group: "nodes", data: { id: i, charger: quickChargers[i] } });
+                counter++;
+            }
+
+            for (var i = 0; i < quickChargers.length; i++) {
+                for (var j = 0; j < quickChargers.length; j++) {
+                    if (i == j)
+                        continue;
+                    cy.add({group: "edges", data: { id: counter, source: i, target: j }})
+                    counter++;
+                }
+            }
+
+            console.log(cy.nodes().size());
+            console.log(cy.edges().size());
+
+            // Montreal - Quebec
+            var src = findChargerByUid(quickChargers, "46897f08-3ea4-4763-8f15-690371c482bc");
+            var dst = findChargerByUid(quickChargers, "379db2c0-821c-4c49-8ff5-88bae15fba11");
+            var result = cy.aStar({root: src, goal: dst, directed: true});
+            console.log(result);
+            done();
+        });
+    });
+
 });
